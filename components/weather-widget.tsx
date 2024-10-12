0ea22fc297d26b97dea5e6fa334d30edf00ec532
Adnan Khan <adnanonline109@gmail.com>
2024-10-13 02:53:18 +0500

    Fixed syntax error in input.tsx

--- components/weather-widget.tsx
+++ components/weather-widget.tsx
@@ -10,7 +10,7 @@ import {
     CardContent 
   } from "@/components/ui/card";
 
-  import { Button  } from "@/components/ui/button";
+  //import { Button  } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 
 import { CloudIcon, MapPinIcon , ThermometerIcon  } from "lucide-react";
